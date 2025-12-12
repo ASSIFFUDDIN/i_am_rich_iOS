dcd0b71c94f007e82a62d9f2efb020741c6989fe
SHAIKH ASSIFFUDDIN <>
2025-12-12 12:25:30 +0530

    init commit

---
+++ i_am_rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i_am_rich
+//
+//  Created by SHAIKH ASSIFFUDDIN on 12/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
